63296a0625d49a55fb9da6fc157644f90f0fe6b0
Mehul Devrani <mehul.devrani@morningstar.com>
2020-03-30 21:23:14 +0530

    Indexing example pascal's triangle extraction

---
+++ pascal_triangle_indexing.py
@@ -0,0 +1,42 @@
+
+class PascalTriangle:
+
+    def __init__(self, num):
+        self.num = num
+        self.list_1 = []
+
+    def populate_list(self):
+        self.list_1.append(1)
+        i = 1
+        
+        while (i <= self.num):
+            k = 1
+            l = []
+            l.append(1)
+            
+            while(k < i):
+                l.append(self.list_1[i - 1][k] + self.list_1[i - 1][k - 1])
+                k = k + 1
+            
+            l.append(1)
+            self.list_1.append(l)
+            i = i + 1
+        
+        return self.list_1   
+
+    def binomial_coeff(self, j):
+        
+        pascal_tri = self.populate_list()
+        print(pascal_tri[9])
+        return (pascal_tri[self.num][j - 1])
+
+    def __str__(self):
+        
+        return f'number selected is {self.num} and pascal triangle is \n {self.populate_list()}'
+
+if __name__ == "__main__":
+    a = PascalTriangle(9)
+    print(a)
+    # print(a.populate_list())
+    print(a.binomial_coeff(6))
+    
