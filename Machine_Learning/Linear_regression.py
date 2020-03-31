36c65ce25f7d26f2eaab274ab78f8d0f84b9654c
Mehul Devrani <mehul.devrani@morningstar.com>
2020-03-31 18:28:29 +0530

    Update linear regression docs

--- Machine_Learning/Linear_regression.py
+++ Machine_Learning/Linear_regression.py
@@ -2,10 +2,17 @@ import numpy as np
 import requests
 
 class LinearRegression:
+    """Runs Linear regeression using gradient steep decent method
+     and calculates the error using sum of sqr error"""
+
     def __init__(self, url):
         self.url = url
 
     def collect_datset(self):
+        """ Collect dataset of CSGO
+    The dataset contains ADR vs Rating of a player.
+    :return : dataset obtained from the link, as matrix
+    """
         response = requests.get(self.url)
         lines = response.text.splitlines()
         data = []
@@ -19,7 +26,9 @@ class LinearRegression:
     def run_steep_gradient_decent(self, data_x, data_y, len_data, alpha, theta):
         """
             Aplha : Learning rate
-            Theta : Feature vector(weight's for our model
+            Theta : Feature vector(weight's for our model)
+            Theta = y - m * x
+            Gradient Decent = Theta - (alpha/n)* d/dQ(Q0, Q1)
         """
         n = len_data
         prod = np.dot(theta, data_x.transpose())
@@ -29,6 +38,10 @@ class LinearRegression:
         return theta
     
     def sum_of_square_error(self, data_x, data_y, len_data, theta):
+        ''' Error = sum of (Q.X - y)^2
+            Error = Error / (2n) for standardization.     
+        '''
+        
         n =len_data
         prod = np.dot(theta, data_x.transpose())
         prod -= data_y.transpose()
@@ -37,13 +50,18 @@ class LinearRegression:
         return error
 
     def run_linear_regression(self, data_x, data_y):
+        '''Runs Linear regression model,
+        calculates theta and error for 
+        best fit line.'''
         
+        # define number of iterations
         iterations = 100000
+        # define learning rate
         alpha = 0.0001550
 
         no_features = data_x.shape[1]
         len_data = data_x.shape[0] - 1
-
+        # initialize zeros matrix of size no of space features
         theta = np.zeros((1, no_features))
 
         for i in range(0, iterations):
@@ -59,7 +77,8 @@ if __name__ == "__main__":
         "https://raw.githubusercontent.com/yashLadha/"
         + "The_Math_of_Intelligence/master/Week1/ADRvs"
         + "Rating.csv"
-    )
+    )  # link to csgo player ratings
+    
     lr = LinearRegression(url)
     data = lr.collect_datset()
     data_x =np.c_[np.ones(len(data)), data[:, :-1]].astype(float)
